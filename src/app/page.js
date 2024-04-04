6c7b5a8f0bb5adf72b5825484b46d846398f8cbc
Gyuyeong <baudaegam@gmail.com>
2024-04-04 15:03:31 +0900

    Find way to always insert code data into DB as well as find a way to parse the URL so that the %20 are parsed to whitespaces and not a + sign

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { ChakraProvider } from "@chakra-ui/react";
 import RunResultSection from "./run-result-section";
 import Script from "next/script";
+import { sql } from "@vercel/postgres";
 
 // Stores problem guide information
 const GuideContainer = ({ titleText, height, text }) => {
@@ -27,25 +28,31 @@ export default async function Home({ params, searchParams }) {
   const longText = "hello";
   const longText2 = "world";
 
+  const codeValue = searchParams["codeValue"] ? decodeURI(searchParams["codeValue"].replace(/\+/g, ' ')) : "";  // it is undefined at the start
+
+  if (codeValue !== "") {
+    await sql`INSERT INTO Code_Results (Code) VALUES (${codeValue})`;  // insert to db
+  }
+
   return (
     <>
       <ChakraProvider>
-          <div className="main">
-            <div className="main-section">
-              <div className="guide-section">
-                <div className="guide-title">문제 & 참조 / 제약사항</div>
-                <GuideContainer titleText={'문제 설명'} height={'200px'} text={longText}></GuideContainer>
-                <GuideContainer titleText={'제한 사항'} height={'200px'} text={longText2}></GuideContainer>
-              </div>
-              <div className="gutter">
-
-              </div>
-              <RunResultSection></RunResultSection>
+        <div className="main">
+          <div className="main-section">
+            <div className="guide-section">
+              <div className="guide-title">문제 & 참조 / 제약사항</div>
+              <GuideContainer titleText={'문제 설명'} height={'200px'} text={longText}></GuideContainer>
+              <GuideContainer titleText={'제한 사항'} height={'200px'} text={longText2}></GuideContainer>
             </div>
+            <div className="gutter">
+
+            </div>
+            <RunResultSection value={codeValue}></RunResultSection>
           </div>
-        </ChakraProvider>
-        <Script src="/skulpt.min.js" type='text/javascript'></Script>
-        <Script src="/skulpt-stdlib.js" type='text/javascript'></Script>
+        </div>
+      </ChakraProvider>
+      <Script src="/skulpt.min.js" type='text/javascript'></Script>
+      <Script src="/skulpt-stdlib.js" type='text/javascript'></Script>
     </>
   );
 }
--- src/app/run-result-section.js
+++ src/app/run-result-section.js
@@ -1,18 +1,17 @@
 'use client'
 
 import React from "react";
-
 import Editor from '@monaco-editor/react';
 import { useState, useRef } from 'react';
 import { Button, SkipNavLink } from '@chakra-ui/react';
 import { useRouter, redirect } from 'next/navigation';
 import { Box } from "@chakra-ui/react";
 import {
-    Accordion,
-    AccordionItem,
-    AccordionButton,
-    AccordionPanel,
-    AccordionIcon,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from '@chakra-ui/react';
 
 function outf(text) {
@@ -29,123 +28,128 @@ function builtinRead(x) {
 
 
 // button for copy and execute
-const EditorButton = ({ text, updateCodeValue, editorRef }) => {
-    const router = useRouter();
+const EditorButton = ({ text, editorRef }) => {
+  const router = useRouter();
 
-    const processCode = () => {
-        const codeValue = editorRef.current.getValue();
+  const processCode = () => {
+    const codeValue = editorRef.current.getValue();  // get code value from editor
 
-        if (codeValue) {
-          updateCodeValue(codeValue);
-          if (text === "실행") {
-            Sk.configure({
-              __future__: Sk.python3  // Python 3
-            });
-            var mypre = document.getElementById("output");
-            mypre.innerHTML = "";
-            Sk.pre = "output";
-            Sk.configure({output:outf, read:builtinRead});
-            (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'turtle_canvas';
-            var myPromise = Sk.misceval.asyncToPromise(function() {
-              return Sk.importMainWithBody("<stdin>", false, codeValue, true);
-            });
-            myPromise.then(function(mod) {
-              console.log("success");
-            },
-              function(err) {
-                console.log(err.toString());
-                alert("Error!!\nCheck Your Code");
-              }
-            );
-          } else {  // copy button
-            console.log(codeValue);
-            navigator.clipboard.writeText(codeValue);  // copy to clipboard
-            alert("Copied to Clipboard");
+    if (codeValue.length > 0) {  // there must be something written there
+      if (text === "실행") {
+        let success = true;  // check if successfully executed
+        Sk.configure({
+          __future__: Sk.python3  // Python 3
+        });
+        var mypre = document.getElementById("output");
+        mypre.innerHTML = "";
+        Sk.pre = "output";
+        Sk.configure({ output: outf, read: builtinRead });
+        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'turtle_canvas';
+        var myPromise = Sk.misceval.asyncToPromise(function () {
+          return Sk.importMainWithBody("<stdin>", false, codeValue, true);
+        });
+        myPromise.then(function (mod) {
+          console.log("success");
+        },
+          function (err) {
+            console.log(err.toString());
+            alert("Error!!\nCheck Your Code");
+            success = false;
           }
-        }
-    }
+        );
 
-    return (
-        <Button size="sm" colorScheme="messenger" onClick={processCode}>{text}</Button>
-    )
+        // push codeValue to server only if there were no errors
+        if (success) {
+          const encodedCodeValue = encodeURI(codeValue);
+          const url = `/?codeValue=${encodedCodeValue}`;
+          console.log(url);
+          router.push(url);
+        }
+
+      } else {  // copy button
+        console.log(codeValue);
+        navigator.clipboard.writeText(codeValue);  // copy to clipboard
+        alert("Copied to Clipboard");
+      }
+    }
+  }
+
+  return (
+    <Button size="sm" colorScheme="messenger" onClick={processCode}>{text}</Button>
+  )
 }
 
 function CodeEditor({ editorRef, codeValue }) {
 
-    const onMount = (editor) => {
-      editorRef.current = editor;
-      editor.focus();
-    }
-  
-    return (
-      <>
-        <Editor 
-          flex="1" 
-          theme='vs-dark'
-          defaultLanguage="python" 
-          value={codeValue} 
-          onMount={onMount}
-          options={{
-            minimap: {enabled: false},
-            scrollbar: {
-              vertical: 'auto',
-              horizontal: 'auto'
-            }
-          }}
-        >
-  
-        </Editor>
-      </>
-    );
+  const onMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  }
+
+  return (
+    <>
+      <Editor
+        flex="1"
+        theme='vs-dark'
+        defaultLanguage="python"
+        value={codeValue}
+        onMount={onMount}
+        options={{
+          minimap: { enabled: false },
+          scrollbar: {
+            vertical: 'auto',
+            horizontal: 'auto'
+          }
+        }}
+      >
+
+      </Editor>
+    </>
+  );
 }
 
 // Stores the results
 const ResultAccordion = ({ title, pre_id, turtle_id }) => {
-    return (
-      <Accordion allowToggle bg="#111" border="none">
-        <AccordionItem>
-          <h2>
-            <AccordionButton bg='#111' color='gray' _expanded={{bg: '#111', color: 'white'}}>
-              <Box as="span" flex='1'textAlign='left' fontSize="large" fontWeight="bold" ml="1px">
-                {title}
-              </Box>
-              <AccordionIcon/>
-            </AccordionButton>
-          </h2>
-          <AccordionPanel bg="#fff" margin='10px'>
-            <pre id={pre_id}>
+  return (
+    <Accordion allowToggle bg="#111" border="none">
+      <AccordionItem>
+        <h2>
+          <AccordionButton bg='#111' color='gray' _expanded={{ bg: '#111', color: 'white' }}>
+            <Box as="span" flex='1' textAlign='left' fontSize="large" fontWeight="bold" ml="1px">
+              {title}
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel bg="#fff" margin='10px'>
+          <pre id={pre_id}>
 
-            </pre>
-            <div id={turtle_id}></div>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-    )
-  }
+          </pre>
+          <div id={turtle_id}></div>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  )
+}
 
-export default function RunResultSection() {
-    const [codeValue, setCodeValue] = useState('');
-    const editorRef = useRef(null);
+export default function RunResultSection({ value }) {
+  const editorRef = useRef(null);
 
-    const updateCodeValue = (value) => {
-        setCodeValue(value);
-    }
-
-    return (
-        <>
-          <div className="run-section">
-            <div className='copy-and-save-section'>
-              <EditorButton text={'Copy'} updateCodeValue={updateCodeValue} editorRef={editorRef}></EditorButton>
-              <EditorButton text={'실행'} updateCodeValue={updateCodeValue} editorRef={editorRef}></EditorButton>
-            </div>
-            <CodeEditor editorRef={editorRef} codeValue={codeValue}></CodeEditor>
-          </div>
-          <div className="result-section">
-            <div className='result-section'>
-              <ResultAccordion title={"실행 결과"} pre_id="output" turtle_id="turtle_canvas"></ResultAccordion>
-              <ResultAccordion title={"코드 구조 및 실행 순서"} pre_id="structure" turtle_id={null}></ResultAccordion>
-            </div>
-          </div>
-        </>
-    )
+  return (
+    <>
+      <div className="run-section">
+        <div className='copy-and-save-section'>
+          <EditorButton text={'Copy'} editorRef={editorRef}></EditorButton>
+          <EditorButton text={'실행'} editorRef={editorRef}></EditorButton>
+        </div>
+        <CodeEditor editorRef={editorRef} codeValue={value}></CodeEditor>
+      </div>
+      <div className="result-section">
+        <div className='result-section'>
+          <ResultAccordion title={"실행 결과"} pre_id="output" turtle_id="turtle_canvas"></ResultAccordion>
+          <ResultAccordion title={"코드 구조 및 실행 순서"} pre_id="structure" turtle_id={null}></ResultAccordion>
+        </div>
+      </div>
+    </>
+  )
 }
